c5bddfdcfc37fb29701d905958279111067b0b4f
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2016-10-18 00:04:02 +0530

    Chapter 1 - Hello World Example

---
+++ chapter01/01-helloworld/src/app.ts
@@ -0,0 +1,18 @@
+import { NgModule, Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+@Component({
+  selector: 'hello-world-app',
+  template: '<h1>Say Hello to Angular 2</h1>'
+})
+class HelloWorldAppComponent { }
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [HelloWorldAppComponent],
+  bootstrap: [HelloWorldAppComponent]
+})
+class HelloWorldAppModule { }
+
+platformBrowserDynamic().bootstrapModule(HelloWorldAppModule);
