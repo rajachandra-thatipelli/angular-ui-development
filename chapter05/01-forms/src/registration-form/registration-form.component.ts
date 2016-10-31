7bd728653174ff3047b9e475af45680513b60b03
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2016-11-01 03:50:20 +0530

    Chapter 5 example 1 template driven forms

---
+++ chapter05/01-forms/src/registration-form/registration-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'registration-form',
+  templateUrl: 'registration-form.component.html'
+})
+export class RegistrationFormComponent {
+}
