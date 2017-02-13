6e63ff654ef46252581a97973a8ea724aaf86f31
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2017-02-13 19:22:49 +0530

    Book store commit#1 routing implementation

---
+++ chapter06/book-store/src/app/books/book-details/book-details.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+
+import 'rxjs/add/operator/switchMap';
+
+import { BookStoreService } from '../book-store.service';
+import { Book } from '../book';
+
+@Component({
+  selector: 'book-details',
+  templateUrl: './book-details.component.html',
+  styleUrls: ['./book-details.component.scss']
+})
+export class BookDetailsComponent implements OnInit {
+
+  book: Book;
+
+  constructor(private storeService: BookStoreService,
+              private route: ActivatedRoute,
+              private location: Location) {
+  }
+
+  ngOnInit(): void {
+    this.route.params
+      .switchMap((params: Params) => this.storeService.getBook(+params['id']))
+      .subscribe(book => this.book = book);
+  }
+
+  goBack() {
+    this.location.back();
+  }
+}
---
+++ chapter06/book-store/src/app/books/new-book/new-book.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { Book } from '../book';
+import { BookStoreService } from '../book-store.service';
+
+@Component({
+  selector: 'new-book',
+  templateUrl: './new-book.component.html',
+  styleUrls: ['./new-book.component.scss']
+})
+export class NewBookComponent implements OnInit {
+
+  newBookForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private storeService: BookStoreService, private router: Router) {
+  }
+
+  ngOnInit() {
+    this.newBookForm = this.formBuilder.group({
+      isbn: ['', Validators.required],
+      title: ['', Validators.required],
+      authors: ['', Validators.required],
+      published: ['', Validators.required],
+      description: ['', Validators.required],
+      coverImage: ['', Validators.required]
+    });
+  }
+
+  saveBook() {
+    if(this.newBookForm.valid) {
+      var book = this.newBookForm.value as Book;
+      this.storeService.addBook(book)
+          .subscribe(res => this.gotoBooksList());
+    }
+  }
+
+  private gotoBooksList() {
+    this.router.navigate(['/books']);
+  }
+}
