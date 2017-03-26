c7c30a9d9dda4621cee4b0090a7f92445730278c
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2017-03-27 03:01:00 +0530

    rxjs examples

--- chapter04/books-search/systemjs.config.js
+++ chapter04/books-search/systemjs.config.js
@@ -9,6 +9,7 @@
       '@angular/compiler': 'ng:compiler/bundles/compiler.umd.js',
       '@angular/platform-browser': 'ng:platform-browser/bundles/platform-browser.umd.js',
       '@angular/platform-browser-dynamic': 'ng:platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/forms': 'ng:forms/bundles/forms.umd.js',
       'rxjs': 'node_modules/rxjs'
     },
     packages: {
--- chapter04/example06/systemjs.config.js
+++ chapter04/example06/systemjs.config.js
@@ -1,15 +1,14 @@
 (function (global) {
   System.config({
     paths: {
-      'angular2:': 'node_modules/@angular/'
+      'ng:': 'node_modules/@angular/'
     },
     map: {
-      '@angular/core': 'angular2:core/bundles/core.umd.js',
-      '@angular/common': 'angular2:common/bundles/common.umd.js',
-      '@angular/compiler': 'angular2:compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'angular2:platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'angular2:platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/forms': 'angular2:forms/bundles/forms.umd.js',
+      '@angular/core': 'ng:core/bundles/core.umd.js',
+      '@angular/common': 'ng:common/bundles/common.umd.js',
+      '@angular/compiler': 'ng:compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'ng:platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'ng:platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
       'rxjs': 'node_modules/rxjs'
     },
     packages: {
