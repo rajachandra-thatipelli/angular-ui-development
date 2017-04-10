fa557bb4d4aac061443d98f679a5a4eed51bf790
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2017-04-10 07:43:23 +0530

    Book store testing

--- chapter07/book-store/src/app/about.component.ts
+++ chapter07/book-store/src/app/about.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'about-page',
-  template: '<h2>This is About Page</h2>',
+  template: `
+    <div>
+      <h4>{{heading}}</h4>
+      <p class="message">{{content}}</p>
+    </div>
+  `,
 })
 export class AboutComponent {
+  heading = 'This is About Page';
+  content = '';
 }
