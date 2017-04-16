e1c2f0cf33b446c238d5d54f2ff82a54f678065d
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2017-04-17 00:53:38 +0530

    Modules updated

--- chapter08/book-store-material-app/src/app/app-material.module.ts
+++ chapter08/book-store-material-app/src/app/app-material.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
-import { MaterialModule } from '@angular/material';
+import {
+  MaterialModule,
+  MdToolbarModule,
+  MdSidenavModule,
+  MdButtonModule,
+  MdIconModule,
+  MdListModule,
+  MdTabsModule,
+  MdProgressSpinnerModule,
+  MdCardModule,
+  MdInputModule,
+  MdDialogModule,
+  MdSnackBarModule
+} from '@angular/material';
+
+const MATERIAL_MODULES = [
+  MdToolbarModule,
+  MdSidenavModule,
+  MdButtonModule,
+  MdIconModule,
+  MdListModule,
+  MdTabsModule,
+  MdProgressSpinnerModule,
+  MdCardModule,
+  MdInputModule,
+  MdDialogModule,
+  MdSnackBarModule
+];
 
 @NgModule({
-  imports: [MaterialModule],
-  exports: [MaterialModule]
+  imports: MATERIAL_MODULES,
+  exports: MATERIAL_MODULES
 })
 export class AppMaterialModule { }
